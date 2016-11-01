2fd278abe4f7b6512766c42db8853a4d54d0dd75
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-11-01 17:03:06 -0200

    First commit

---
+++ test/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import chai from 'chai'
+import chaiAsPromised = from 'chai-as-promised';
+chai.use(chaiAsPromised);
+chai.should()
+
+const expect = chai.expect
+const assert = chai.assert
+
+describe('Sky Biometry Client', function() {
+    it('should build correct string for user@namespace', function() {
+
+    })
+});
