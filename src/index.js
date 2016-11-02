5bdf072cb15c1328fd14941d96fdfae31c31bc60
rafaelcorreiapoli <rafael.correia.poli@gmail.com>
2016-11-02 01:04:06 -0200

    Added tests

---
+++ src/index.js
@@ -0,0 +1,125 @@
+import { Map, fromJS } from 'immutable'
+
+const SET_SEARCH_TEXT = 'dashboard/SET_SEARCH_TEXT'
+const SET_PAGE = 'dashboard/SET_PAGE'
+const SET_SORT = 'dashboard/SET_SORT'
+const SET_FILTER = 'dashboard/SET_FILTER'
+const SET_RESULTS = 'dashboard/SET_RESULTS'
+const SET_PAGE_SIZE = 'dashboard/SET_PAGE_SIZE'
+const REGISTER_SEARCH = 'dashboard/REGISTER_SEARCH'
+
+export const getSearchText = (state, searchId) => state.dashboard.getIn([searchId, 'searchText'])
+export const getPage = (state, searchId) => state.dashboard.getIn([searchId, 'page'])
+export const getFilter = (state, searchId) => state.dashboard.getIn([searchId, 'filter'])
+export const getSort = (state, searchId) => state.dashboard.getIn([searchId, 'sort'])
+export const getResults = (state, searchId) => state.dashboard.getIn([searchId, 'results'])
+export const getPageSize = (state, searchId) => state.dashboard.getIn([searchId, 'pageSize'])
+
+export const newSearch = (searchId, searchFn) => (dispatch, getState) => {
+  const state = getState()
+  const page = getPage(state, searchId)
+  const searchText = getSearchText(state, searchId)
+  const filter = getFilter(state, searchId)
+  const sort = getSort(state, searchId)
+  const pageSize = getPageSize(state, searchId)
+
+  console.log('calling new search!')
+  searchFn && searchFn({
+    page,
+    searchText,
+    filter,
+    sort,
+    pageSize,
+  })
+}
+export const setSearchText = (searchId, searchText) => ({
+  type: SET_SEARCH_TEXT,
+  payload: {
+    searchId,
+    searchText,
+  },
+})
+export const setPage = (searchId, page) => ({
+  type: SET_PAGE,
+  payload: {
+    searchId,
+    page,
+  },
+})
+export const setSort = (searchId, sort) => ({
+  type: SET_SORT,
+  payload: {
+    searchId,
+    sort,
+  },
+})
+export const setFilter = (searchId, filter) => ({
+  type: SET_FILTER,
+  payload: {
+    searchId,
+    filter,
+  },
+})
+
+export const setPageSize = (searchId, pageSize) => ({
+  type: SET_PAGE_SIZE,
+  payload: {
+    searchId,
+    pageSize,
+  },
+})
+export const setResults = (searchId, results) => ({
+  type: SET_RESULTS,
+  payload: {
+    searchId,
+    results,
+  },
+})
+
+export const registerSearch = (searchId, initialState) => ({
+  type: REGISTER_SEARCH,
+  payload: {
+    searchId,
+    initialState,
+  }
+})
+
+
+const searchShape = {
+  searchText: '',
+  page: 0,
+  sort: 'nome.asc',
+  filter: {
+
+  },
+  results: {},
+  pageSize: 10,
+}
+
+const initialState = Map({})
+
+export default (state = initialState, action) => {
+  if (!action.payload || !action.payload.searchId) return state
+  const {
+    searchId,
+  } = action.payload
+
+  switch (action.type) {
+    case SET_SEARCH_TEXT:
+      return state.setIn([searchId, 'searchText'], action.payload.searchText)
+    case SET_PAGE:
+      return state.setIn([searchId, 'page'], action.payload.page)
+    case SET_SORT:
+      return state.setIn([searchId, 'sort'], action.payload.sort)
+    case SET_FILTER:
+      return state.setIn([searchId, 'filter'], fromJS(action.payload.filter))
+    case SET_RESULTS:
+      return state.setIn([searchId, 'results'], fromJS(action.payload.results))
+    case SET_PAGE_SIZE:
+      return state.setIn([searchId, 'pageSize'], action.payload.pageSize)
+    case REGISTER_SEARCH:
+      return state.set(searchId, fromJS(action.payload.initialState || searchShape))
+    default:
+      return state
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,15 +1,256 @@
 'use strict';
 
 import chai from 'chai'
-import chaiAsPromised = from 'chai-as-promised';
+import chaiAsPromised from 'chai-as-promised';
+import chaiImmutable from 'chai-immutable';
+import { Map, fromJS } from 'immutable'
+
+chai.use(chaiImmutable);
 chai.use(chaiAsPromised);
 chai.should()
 
 const expect = chai.expect
 const assert = chai.assert
 
-describe('Sky Biometry Client', function() {
-    it('should build correct string for user@namespace', function() {
+import reducer, { setSearchText, setPageSize, setPage, setSort, registerSearch, setFilter, setResults } from '../src/index.js'
 
+const state = fromJS({
+  search1: {
+    searchText: '',
+    page: 0,
+    sort: 'nome.asc',
+    filter: {},
+    results: {},
+    pageSize: 10,
+  }
+})
+
+describe('Dashboard', function() {
+  describe('Reducer', function() {
+    it('initialState', function() {
+      const actual = reducer(undefined, {})
+      const expected = Map({})
+      assert.equal(actual, expected)
     })
+    it('setPageSize', function() {
+      const action = setPageSize('search1', 20)
+      const nextState = reducer(state, {})
+
+      const expected = fromJS({
+        search1: {
+          searchText: '',
+          page: 0,
+          sort: 'nome.asc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        }
+      })
+
+      assert.deepEqual(nextState, expected)
+    })
+
+    it('setPage', function() {
+      const action = setPage('search1', 1)
+      const nextState = reducer(state, action)
+
+      expect(nextState).to.equal(fromJS({
+        search1: {
+          searchText: '',
+          page: 1,
+          sort: 'nome.asc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        }
+      }))
+    })
+
+
+    it('setSort', function() {
+      const action = setSort('search1', 'nome.desc')
+      const nextState = reducer(state, action)
+
+      expect(nextState).to.equal(fromJS({
+        search1: {
+          searchText: '',
+          page: 0,
+          sort: 'nome.desc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        }
+      }))
+    })
+
+    it('registerSearch', function() {
+      const action = registerSearch('search2')
+      const nextState = reducer(state, action)
+
+      expect(nextState).to.equal(fromJS({
+        search1: {
+          searchText: '',
+          page: 0,
+          sort: 'nome.asc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        },
+        search2: {
+          searchText: '',
+          page: 0,
+          sort: 'nome.asc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        }
+      }))
+    })
+
+
+    it('setFilter', function() {
+      const action = setFilter('search1', {})
+      const nextState = reducer(state, action)
+
+      expect(nextState).to.equal(fromJS({
+        search1: {
+          searchText: '',
+          page: 0,
+          sort: 'nome.asc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        },
+      }))
+    })
+
+
+    it('setResults', function() {
+      const action = setResults('search1', {
+        id1: {
+          name: 'rafa'
+        }
+      })
+
+      const nextState = reducer(state, action)
+
+      expect(nextState).to.equal(fromJS({
+        search1: {
+          searchText: '',
+          page: 0,
+          sort: 'nome.asc',
+          filter: {},
+          results: {
+            id1: {
+              name: 'rafa'
+            }
+          },
+          pageSize: 10,
+        },
+      }))
+    })
+
+    it('setSearchText', function() {
+      const action = setSearchText('search1', 'ra')
+      const nextState = reducer(state, action)
+
+      expect(nextState).to.equal(fromJS({
+        search1: {
+          searchText: 'ra',
+          page: 0,
+          sort: 'nome.asc',
+          filter: {},
+          results: {},
+          pageSize: 10,
+        },
+      }))
+    })
+  })
+  describe('Action Creators', function() {
+    it('setSearchText', function() {
+      const action = setSearchText('search1', 'ra')
+
+      assert.deepEqual(action, {
+        type: 'dashboard/SET_SEARCH_TEXT',
+        payload: {
+          searchId: 'search1',
+          searchText: 'ra'
+        }
+      })
+    })
+
+    it('setPageSize', function() {
+      const action = setPageSize('search1', 10)
+
+      assert.deepEqual(action, {
+        type: 'dashboard/SET_PAGE_SIZE',
+        payload: {
+          searchId: 'search1',
+          pageSize: 10
+        }
+      })
+    })
+
+    it('setPage', function() {
+      const action = setPage('search1', 1)
+
+      assert.deepEqual(action, {
+        type: 'dashboard/SET_PAGE',
+        payload: {
+          searchId: 'search1',
+          page: 1
+        }
+      })
+    })
+
+    it('setSort', function() {
+      const action = setSort('search1', 'nome.asc')
+
+      assert.deepEqual(action, {
+        type: 'dashboard/SET_SORT',
+        payload: {
+          searchId: 'search1',
+          sort: 'nome.asc'
+        }
+      })
+    })
+
+    it('setFilter', function() {
+      const action = setFilter('search1', {
+        age: [0, 10]
+      })
+
+      assert.deepEqual(action, {
+        type: 'dashboard/SET_FILTER',
+        payload: {
+          searchId: 'search1',
+          filter: {
+            age: [0, 10]
+          }
+        }
+      })
+    })
+
+    it('setResults', function() {
+      const action = setResults('search1', {
+        id1: {
+          name: 'rafa'
+        }
+      })
+
+      assert.deepEqual(action, {
+        type: 'dashboard/SET_RESULTS',
+        payload: {
+          searchId: 'search1',
+          results: {
+            id1: {
+              name: 'rafa'
+            }
+          }
+        }
+      })
+    })
+
+
+  })
 });
